cb9a8e741c7362ed5374560f9571995212297257
Rajesh <rajesh.s@vrinsoft.com>
2023-10-20 16:12:21 +0530

    Added Datepicker class without Done callback

---
+++ Example/Pods/Target Support Files/RDatePicker/RDatePicker-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_RDatePicker : NSObject
+@end
+@implementation PodsDummy_RDatePicker
+@end
--- README.md
+++ README.md
@@ -22,7 +22,7 @@ pod 'RDatePicker'
 
 ## Author
 
-imrajeshcoder, rajesh.s@vrinsoft.com
+imrajeshcoder, imrajeshcoder@gmail.com
 
 ## License
 
